7cfba51da41a990384113b247c1aa9a42311cdb5
ryanschwarzkopf <r.schwarzkopf20@gmail.com>
2023-05-10 22:08:44 -0600

    add grid, modify search

--- TBD/linked_list.js
+++ TBD/linked_list.js
@@ -2,25 +2,48 @@ function init() {
     const $ = go.GraphObject.make;
     myDiagram = $(go.Diagram, "diagramDiv");
 
+    myDiagram.isReadOnly = true;
+
+    myDiagram.grid =
+    $(go.Panel, "Grid",
+    {
+        name: "GRID",
+        visible: false,
+        gridCellSize: new go.Size(10, 10),
+        gridOrigin: new go.Point(0, 0)
+    },
+        $(go.Shape, "LineH", { stroke: "lightgray", strokeWidth: 0.5, interval: 1 }),
+        $(go.Shape, "LineH", { stroke: "gray", strokeWidth: 0.5, interval: 5 }),
+        $(go.Shape, "LineH", { stroke: "gray", strokeWidth: 1.0, interval: 10 }),
+        $(go.Shape, "LineV", { stroke: "lightgray", strokeWidth: 0.5, interval: 1 }),
+        $(go.Shape, "LineV", { stroke: "gray", strokeWidth: 0.5, interval: 5 }),
+        $(go.Shape, "LineV", { stroke: "gray", strokeWidth: 1.0, interval: 10 })
+    );
+
+    myDiagram.grid.visible = true;  // so that this example shows the standard grid
+    myDiagram.div.style.background = "white";
+
     var nodeDataArray = [];
     var linkDataArray = [];
     keyValue = 0;
     myDiagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
+    
     myDiagram.nodeTemplate =
-        $(go.Node, "Auto",
-            $(go.Shape, "RoundedRectangle", { fill: "white"},
-                new go.Binding("fill", "color")),
-            $(go.TextBlock,
-                new go.Binding("text", "say"))
-        );
-    myDiagram.linkTemplate =
-        $(go.Link,
-            $(go.Shape, { strokeWidth: 2},
-                new go.Binding("stroke", "color")),
-            $(go.Shape, { toArrow: "Standard", stroke: null },
-                new go.Binding("fill", "color"))
-        );
+        new go.Node("Auto")
+        .bind(new go.Binding("location", "loc"))
+        .add(new go.Shape("RoundedRectangle", { margin: 5, fill: "white" }))
+        .add(new go.TextBlock({ margin: 5 }).bind(new go.Binding("text", "say")));
 
+    myDiagram.linkTemplate =
+        new go.Link()
+          .add(new go.Shape()
+              .bind(new go.Binding("strokeWidth", "", function() { return 0; }))
+              .bind(new go.Binding("stroke", "color")))
+          .add(new go.Shape()
+              .bind(new go.Binding("toArrow", "", function() { return "Standard"; }))
+              .bind(new go.Binding("fill", "color"))
+              .bind(new go.Binding("stroke", "", function() { return null; })));
+      
     var insertButton = document.getElementById("Insert_Button");
     insertButton.addEventListener("click", 
         function() {
@@ -31,8 +54,10 @@ function init() {
             }
             addNode(myDiagram.model, inputVal);
         });
+    
     var removeButton = document.getElementById("Remove_Button");
     removeButton.addEventListener("click", removeNode(myDiagram.model));
+    
     var searchButton = document.getElementById("Search_Button");
     searchButton.addEventListener("click", function() {
         var inputVal = document.getElementById("myInput").value;
@@ -40,16 +65,16 @@ function init() {
             alert("Invalid input");
             return;
         }
-        searchNode(myDiagram, inputVal);
+        searchNode(myDiagram, inputVal, linkDataArray);
     });
 } // end init
 
 function addNode(model, input) {
     if(input == "") return; // don't make any empty nodes
-    var newNode = { key: keyValue.toString(), say: input.toString(), position: new go.Point(100*keyValue, 0)};
+    var newNode = { key: keyValue.toString(), say: input.toString(), loc: new go.Point(Math.random() * document.body.offsetWidth - 300, Math.random() * document.documentElement.scrollHeight - 300 ) };
     keyValue += 1;
     model.addNodeData(newNode);
-    if(model.length !== 1) { // if length is one, don't add a link
+    if(model.nodeDataArray[model.nodeDataArray.length - 2] !== undefined) { // if length is one, don't add a link
         var prevNode = model.nodeDataArray[model.nodeDataArray.length - 2];
         var newLink = { to: newNode.key, from: prevNode.key }
         model.addLinkData(newLink);
@@ -61,11 +86,26 @@ function removeNode(model) {
     model.removeNodeData(model.findNodeForKey());
 }
 
-function searchNode(diagram, input) {
-    var node = diagram.findNodeForData({ say: input.toString() });
-    if(node === null) {console.log(diagram.model.nodeDataArray);}
-    if(node !== null) {
-        alert("found");
-        diagram.model.setDataProperty(node.data, "color", "red");    
-    }
+async function searchNode(myDiagram, input) {
+    console.log("start");
+    await delay(1000); // Wait for 3 seconds
+    console.log("stop");
+
+    // Iterate through the link data array
+    myDiagram.links.each(async function(link) {
+        // Set the stroke width of the link to 1
+        link.strokeWidth = 2;
+        // Update the model to show the new stroke width
+        myDiagram.model.updateTargetBindings(link, "strokeWidth");
+        // Pause for one second using a helper function
+        delay(1000);
+        // Set the stroke width of the link back to 0
+        link.strokeWidth = 0;
+        // Update the model to show the original stroke width
+        myDiagram.model.updateTargetBindings(link, "strokeWidth");
+    });
 }
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
